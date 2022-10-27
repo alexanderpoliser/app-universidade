cba4a61e5a4405e1a8c7d91e3e2ed0eb85657db3
AlexanderPoliser <alexanderpoliser@gmail.com>
2022-10-26 23:23:56 -0300

    feat(page: Aluno): added input style & validation to prevent empty data submissions

--- app/pages/Aluno/Aluno.tsx
+++ app/pages/Aluno/Aluno.tsx
@@ -1,71 +1,106 @@
 import React from "react";
-import { View, Text, Button, TextInput } from "react-native";
-import { getFirestore, collection, getDocs, doc, setDoc, addDoc} from 'firebase/firestore'
+import { View, Button, TextInput, StyleSheet } from "react-native";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import db from "../../../services/firebase/firebase";
 
-const collecRef = collection(db, 'Aluno');
+const collecRef = collection(db, "Aluno");
 
 export default function App() {
-    function cadastrarAluno() {
-        console.log(nome);
-        addDoc(collection(db, "Aluno"), {
-        nome: nome,
-        foto: foto,
-        cidade: cidade,
-        endereco: endereco
-        });
-        loadAluno();
-    }
-    
-    function loadAluno() {
-      getDocs(collecRef)
-        .then( (snapshot)=> {
-          for(let i=0;i<snapshot.docs.length;i++) {
-            console.log('Dados[', i, ']: ', snapshot.docs[i].data())
-          }
-        }).catch(err => {
-        console.log(err.message)
-      })
-    }
+  const [nome, setNome] = React.useState("");
+  const [foto, setFoto] = React.useState("");
+  const [cidade, setCidade] = React.useState("");
+  const [endereco, setEndereco] = React.useState("");
 
-    const [nome, setNome] = React.useState("");
-    const [foto, setFoto] = React.useState("");
-    const [cidade, setCidade] = React.useState("");
-    const [endereco, setEndereco] = React.useState("");
+  function cadastrarAluno() {
+    addDoc(collection(db, "Aluno"), {
+      nome: nome,
+      foto: foto,
+      cidade: cidade,
+      endereco: endereco,
+    });
+    window.alert("Aluno cadastrado!");
+    loadAluno();
+  }
+
+  function loadAluno() {
+    getDocs(collecRef)
+      .then((snapshot) => {
+        for (let i = 0; i < snapshot.docs.length; i++) {
+          console.log("Dados[", i, "]: ", snapshot.docs[i].data());
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  React.useEffect(() => {
+    loadAluno();
+  }, []);
+
+  function validaCampos() {
+    if (nome == "") {
+      window.alert("Insira um nome válido!");
+    } else if (foto == "") {
+      window.alert("Insira uma foto válida!");
+    } else if (cidade == "") {
+      window.alert("Insira uma cidade válida!");
+    } else if (endereco == "") {
+      window.alert("Insira um endereço válido!");
+    } else cadastrarAluno();
+  }
 
   return (
     <View>
-        <TextInput
-         value={nome}
-         placeholder="Digite seu nome!" 
-         style={{borderWidth:2, borderColor:"red"}}
-         onChangeText={(text) => {setNome(text)}}
-        />
-        <TextInput
-         value={foto}
-         placeholder="Digite o link para sua foto!" 
-         style={{borderWidth:2, borderColor:"red"}}
-         onChangeText={(text) => {setFoto(text)}}
-        />
-        <TextInput
-         value={cidade}
-         placeholder="Digite sua cidade!" 
-         style={{borderWidth:2, borderColor:"red"}}
-         onChangeText={(text) => {setCidade(text)}}
-        />
-        <TextInput
-         value={endereco}
-         placeholder="Digite seu endereço!" 
-         style={{borderWidth:2, borderColor:"red"}}
-         onChangeText={(text) => {setEndereco(text)}}
-        />
+      <TextInput
+        style={styles.TextInput}
+        value={nome}
+        placeholder="Digite seu nome!"
+        onChangeText={(text) => {
+          setNome(text);
+        }}
+      />
+      <TextInput
+        value={foto}
+        style={styles.TextInput}
+        placeholder="Digite o link para sua foto!"
+        onChangeText={(text) => {
+          setFoto(text);
+        }}
+      />
+      <TextInput
+        value={cidade}
+        style={styles.TextInput}
+        placeholder="Digite sua cidade!"
+        onChangeText={(text) => {
+          setCidade(text);
+        }}
+      />
+      <TextInput
+        value={endereco}
+        style={styles.TextInput}
+        placeholder="Digite seu endereço!"
+        onChangeText={(text) => {
+          setEndereco(text);
+        }}
+      />
 
-        <Button
-        onPress={cadastrarAluno}
+      <Button
+        onPress={validaCampos}
         title="Cadastrar Aluno"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-        />
+        color="#2196f3"
+        accessibilityLabel="Cadastrar Aluno"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  TextInput: {
+    borderWidth: 3,
+    borderColor: "#D9D9D9",
+    borderRadius: 5,
+    margin: 10,
+    padding: 3,
+  },
+});
--- app/pages/Menu/Menu.tsx
+++ app/pages/Menu/Menu.tsx
@@ -1,31 +1,30 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { NavigationContainer, NavigationContainerProps } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Aluno from "../Aluno/Aluno";
 
 const Stack = createNativeStackNavigator();
 
-function Menu(props){
-  return(
+function Menu(props: any) {
+  return (
     <View>
-        <Button
-                style = {{backgroundColor:'blue'}}
-                title = "Aluno"
-                onPress = {()=>props.navigation.navigate('Aluno')} 
-        />
-    </View>    
+      <Button
+        title="Aluno"
+        onPress={() => props.navigation.navigate("Aluno")}
+        color="#2196f3"
+      />
+    </View>
   );
 }
 
-export default function App(){
-    return(
-
-          <NavigationContainer independent={true}>
-              <Stack.Navigator>
-                  <Stack.Screen name='Menu' component={Menu}/>
-                  <Stack.Screen name='Aluno' component={Aluno}/>
-              </Stack.Navigator>
-          </NavigationContainer>
-      )
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="Aluno" component={Aluno} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
