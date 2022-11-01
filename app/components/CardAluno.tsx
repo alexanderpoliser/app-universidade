7a304f94d0c8efde193ee4b71aaa3d3620a7a643
AlexanderPoliser <alexanderpoliser@gmail.com>
2022-10-31 22:04:55 -0300

    refactor(indentation): refactor code identation to to improve readability

--- app/components/CardAluno.tsx
+++ app/components/CardAluno.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
-import CardAluno from "../models/Aluno";
+import Aluno from "../models/Aluno";
 
-export default function App(props: CardAluno) {
-    return(
-        <View style={{flex:1,
-            alignItems:"center",
-            backgroundColor:"#D9D9D9",
-            padding:20,
-            margin:20,
-            borderRadius:20}}>
+export default function App(props: Aluno) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#D9D9D9",
+        padding: 20,
+        margin: 20,
+        borderRadius: 20,
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <Image
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: 50,
+            marginBottom: 10,
+          }}
+          source={{ uri: props.foto as any }}
+        />
+      </View>
 
-            <View style={{flex:1}}>
-                <Image 
-                style={{width: '100px',
-                 height: '100px',
-                borderRadius:50,
-                marginBottom:10}}
-                source={{uri:props.foto as any}}/>
-            </View>
-
-            <Text style={{fontWeight:'500'}}>{props.nome}</Text>
-            <Text>{props.cidade}</Text>
-        </View>
-    )
+      <Text style={{ fontWeight: "500" }}>{props.nome}</Text>
+      <Text>{props.cidade}</Text>
+    </View>
+  );
 }
-
