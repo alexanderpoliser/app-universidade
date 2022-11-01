34f9813e517512860e924aa5e914592f0ac30ad7
AlexanderPoliser <alexanderpoliser@gmail.com>
2022-10-31 22:10:03 -0300

    feat(component): added a Card Professor component to display teacher information

---
+++ app/components/CardProfessor.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Professor from "../models/Professor";
+
+export default function App(props: Professor) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#D9D9D9",
+        padding: 20,
+        margin: 20,
+        borderRadius: 20,
+      }}
+    >
+      <Text style={{ fontWeight: "500" }}>{props.nome}</Text>
+      <Text>{props.cidade}</Text>
+    </View>
+  );
+}
